533506bdaba394d35498d12660fc3f0546e3079b
Logan Hasson <logan.hasson@gmail.com>
2013-10-15 14:36:53 -0400

    Completed RSpec tests.

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,19 +1,27 @@
 require_relative 'spec_helper.rb'
 
 describe "Song" do
+
+  let(:song) { Song.new }
+
   it "can initialize a song" do
-    pending #implement this spec
+    song.should be_a(Song)
   end
 
   it "can have a name" do
-    pending #implement this spec
+    song.name = "Jump Around"
+    song.name.should eq("Jump Around")
   end
 
   it "can have a genre" do
-    pending #implement this spec
+    song.genre = Genre.new.tap {|g| g.name = "rap"}
+    song.genre.name.should eq("rap")
+    song.genre.should be_a(Genre)
   end
 
   it "has an artist" do
-    pending #implement this spec
+    song.artist = Artist.new.tap {|a| a.name = "miley"}
+    song.artist.should be_a(Artist)
+    song.artist.name.should eq("miley")
   end
 end
