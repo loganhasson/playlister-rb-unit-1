7f58e2c1294eb57541db71c3e3f270395a8203c1
ivanbrennan <ivan.brennan@gmail.com>
2013-10-15 21:25:47 -0400

    Note unwanted require statements in lib files.
    Instructions indicate that none of our lib files should have any require
    or require_relative statements. Commented on this in several of our
    model files which are requiring environment.rb
    
    Commented a question about how we use Dir.foreach in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,13 @@ require 'erb'
 
 ProjectRoot ||= "#{File.dirname(__FILE__)}/.."
 
+# why aren't we passing ("../lib/concerns") here?
 Dir.foreach("lib/concerns") do |file|
   next if file.start_with?('.')
   require_relative "../lib/concerns/#{file}" if file.end_with?(".rb")
 end
 
+# samy question as line 7
 Dir.foreach("lib/models") do |file|
   next if file.start_with?('.')
   require_relative "../lib/models/#{file}" if file.end_with?(".rb")
--- lib/models/song.rb
+++ lib/models/song.rb
@@ -1,3 +1,5 @@
+# the instructions seem to indicate we don't use
+# any require or require_relative statements here
 require_relative '../../config/environment'
 
 class Song
